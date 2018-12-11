bbe50939a5ee33400a9bf9bad78271b45b946b03
zzde <zhangxh1997@gmail.com>
2018-12-11 23:33:37 +0800

    update

--- src/main/web/static/js/wsocket.js
+++ src/main/web/static/js/wsocket.js
@@ -28,4 +28,24 @@ var ws ={
         }
     },
 
-}
+    registerReceive: function() {
+        console.log("userId为 " + userId + " 的用户登记到在线用户表成功！");
+    },
+
+    sendGroups: function (data) {
+        var group = data.groups;
+        var groupListHTML = "";
+        for (var i = 0; i < group.length; i++){
+            groupListHTML +=
+                '<li>' +
+                '<div class="liLeft"><img src="static/img/emoji/emoji_03.png"></div>' +
+                '<div class="liRight">' +
+                '<span class="hidden-groupId">' + group[i].groupId + '</span>' +
+                '<span class="intername">' + group[i].groupName + '</span>' +
+                '<span class="infor"></span>' +
+                '</div>' +
+                '</li>';
+        }
+            $("#groups").append(groupListHTML);
+    }
+};
